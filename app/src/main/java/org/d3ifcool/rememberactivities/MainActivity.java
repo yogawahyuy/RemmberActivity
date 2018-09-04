92328398c2eea6b87ed88134adffe660b08e886a
Sipa <nurasysyifaufissudur@gmail.com>
2018-09-04 12:39:36 +0700

    ini dari anggota satu tambahan komentar tidak penting

--- app/src/main/java/org/d3ifcool/rememberactivities/MainActivity.java
+++ app/src/main/java/org/d3ifcool/rememberactivities/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ini adalah punya silfi, silfi enak dong ada beban biar kurus
     }
 }
