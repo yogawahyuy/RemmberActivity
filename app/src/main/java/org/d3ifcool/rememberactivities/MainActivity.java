84ec24d053aeb34f18c918b4a6ecfbbcb2d28c41
Praktikan <nurasysyifaufissudur@gmail.com>
2018-09-18 13:12:57 +0700

    edit layout home dan main

--- app/src/main/java/org/d3ifcool/rememberactivities/MainActivity.java
+++ app/src/main/java/org/d3ifcool/rememberactivities/MainActivity.java
@@ -1,15 +1,28 @@
 package org.d3ifcool.rememberactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView klikGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ini adalah punya silfi, silfi enak dong ada beban biar kurus hahhaahaha
+        klikGoogle = (TextView)findViewById(R.id.klik);
+        klikGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
     //yoga comit eaa
 }
