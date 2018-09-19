c465b558dd4f7b545d85c18ab1ecb1f880451e52
Praktikan <nurasysyifaufissudur@gmail.com>
2018-09-19 15:39:47 +0700

    menambahkan floating action button

--- app/src/main/java/org/d3ifcool/rememberactivities/HomeActivity.java
+++ app/src/main/java/org/d3ifcool/rememberactivities/HomeActivity.java
@@ -1,15 +1,28 @@
 package org.d3ifcool.rememberactivities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FloatingActionButton tambah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tambah = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+        tambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,TambahKegiatanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
