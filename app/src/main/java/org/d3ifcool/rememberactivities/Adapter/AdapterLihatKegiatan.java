be832c7772022ec3d546000dfdbed82e26b25d0b
Yogawahyuy4058 <37763772+Yogawahyuy4058@users.noreply.github.com>
2018-09-19 15:53:23 +0700

    Menambahkan Adapter

---
+++ app/src/main/java/org/d3ifcool/rememberactivities/Adapter/AdapterLihatKegiatan.java
@@ -0,0 +1,8 @@
+package org.d3ifcool.rememberactivities.Adapter;
+
+/**
+ * Created by Yoga Wahyu Yuwono on 19/09/2018.
+ */
+
+public class AdapterLihatKegiatan {
+}
---
+++ app/src/main/java/org/d3ifcool/rememberactivities/Adapter/AdapterPencapaian.java
@@ -0,0 +1,31 @@
+package org.d3ifcool.rememberactivities.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Yoga Wahyu Yuwono on 19/09/2018.
+ */
+
+public class AdapterPencapaian extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
